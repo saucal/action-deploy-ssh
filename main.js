c54c457fc73c9c85bf3c21eb3cd562d3222ca5d9
Kostas Seresiotis <kostas@saucal.com>
2023-05-01 22:57:46 +0300

    initial action

---
+++ main.js
@@ -0,0 +1,32 @@
+
+
+(async function() {
+
+	const core = require('@actions/core');
+	const Rsync = require('rsync');
+
+	const remoteTarget = core.getInput('env-user', { required: true }) + '@' + core.getInput('env-host', { required: true })
+	const remotePort = core.getInput('env-port', { required: false })
+
+	var rsync = new Rsync()
+	  .shell('ssh')
+	  .flags( core.getInput( 'env-ssh-flags', {require: true }) )
+	  .source( core.getInput('env-local-root', { required: true } ) )
+	  .destination( remoteTarget + ':' + core.getInput('env-remote-root', { required: true }));
+	
+	rsync.debug( true ); // core.isDebug()
+
+	console.log( rsync.command() );
+
+	if ( remotePort && remotePort != 22 ) {
+		rsync.set('shell', 'ssh -p ' + remotePort );
+	}
+
+	// Execute the command
+	rsync.execute(function(error, code, cmd) {
+		// we're done
+		console.log( 'rsync error: ' + error );
+		console.log( 'rsync code: ' + code );
+	});
+
+})()
