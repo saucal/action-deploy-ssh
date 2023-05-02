c8a7aea974d0e83bafaa3fbc43aeb5677e16602a
Kostas Seresiotis <kostas@saucal.com>
2023-05-02 19:37:39 +0300

    parse ignoreList + add other rsync params

--- main.js
+++ main.js
@@ -1,38 +1,79 @@
+(async function () {
+  const core = require("@actions/core");
+  const Rsync = require("rsync");
 
+  const remoteTarget =
+	core.getInput("env-user", { required: true }) +
+	"@" +
+	core.getInput("env-host", { required: true });
+  const remotePort = core.getInput("env-port", { required: false });
+  let shellParams = core.getInput("shell-params", { required: false });
+  let ignoreList = core.getInput("force-ignore", { required: false });
 
-(async function() {
+  let includes,
+	excludes = [];
 
-	const core = require('@actions/core');
-	const Rsync = require('rsync');
+  if (shellParams) {
+	shellParams = shellParams.split(" ");
+  } else {
+	shellParams = ["-oStrictHostKeyChecking=no"];
+  }
 
-	const remoteTarget = core.getInput('env-user', { required: true }) + '@' + core.getInput('env-host', { required: true })
-	const remotePort = core.getInput('env-port', { required: false })
+  if (remotePort) {
+	shellParams.push("-p " + remotePort);
+  }
 
-	var rsync = new Rsync()
-	  .shell( 'ssh -oStrictHostKeyChecking=no -p ' + remotePort ) // -i /home/runner/.ssh/github_actions -vv
-	  .flags( core.getInput( 'env-ssh-flags', {require: true }) )
-	  .source( core.getInput('env-local-root', { required: true } ) )
-	  .destination( remoteTarget + ':' + core.getInput('env-remote-root', { required: true }));
-	
-	rsync.debug( true ); // core.isDebug()
+  if (ignoreList) {
+	// Split ignore list by newlines
+	ignoreList = ignoreList.split("\n");
 
-	console.log( rsync.command() );
+	for (let i = 0; i < ignoreList.length; i++) {
+	  // If starts with a !, include
+	  if (ignoreList[i].startsWith("!")) {
+		includes.push(ignoreList[i].substr(1, ignoreList[i].length - 1));
+		continue;
+	  }
 
-	// Execute the command
-	rsync.execute(function(error, code, cmd) {
-		// we're done
-		if ( code != 0 ) {
-			console.error( 'rsync error: ' + error );
-			console.error( 'rsync code: ' + code );
-			core.setFailed( 'rsync failed with code ' + code );
-		}
-	}, function(data){
-        // do things like parse progress
-		console.log( data.toString() );
-    }, function(data) {
-        // do things like parse error output
-		console.error( data.toString() );
-    }
+	  // Otherwise, exclude
+	  excludes.push(ignoreList[i]);
+	}
+  }
+
+  var rsync = new Rsync()
+	.shell("ssh " + shellParams.join(" ")) // -i /home/runner/.ssh/github_actions -vv
+	.flags(core.getInput("env-ssh-flags", { require: true }))
+	.source(core.getInput("env-local-root", { required: true }))
+	.destination(
+	  remoteTarget + ":" + core.getInput("env-remote-root", { required: true })
 	);
 
-})()
+  rsync.set("delete");
+  rsync.set("no-inc-recursive");
+
+  rsync.include(includes);
+  rsync.exclude(excludes);
+
+  rsync.debug(true); // core.isDebug()
+
+  console.log(rsync.command());
+
+  // Execute the command
+  rsync.execute(
+	function (error, code, cmd) {
+	  // we're done
+	  if (code != 0) {
+		console.error("rsync error: " + error);
+		console.error("rsync code: " + code);
+		core.setFailed("rsync failed with code " + code);
+	  }
+	},
+	function (data) {
+	  // do things like parse progress
+	  console.log(data.toString());
+	},
+	function (data) {
+	  // do things like parse error output
+	  console.error(data.toString());
+	}
+  );
+})();
