d9503aafe0a99545386318382872daaa057551ee
Matias Saggiorato <matiassaggiorato@gmail.com>
2023-05-26 19:09:58 -0300

    Do not use copilot bs

--- pre.js
+++ pre.js
@@ -6,8 +6,14 @@
 
 	const remoteHost = core.getInput( 'env-host', { required: true } );
 	const remotePort = core.getInput( 'env-port', { required: false } );
-	exec.exec( 'ssh-keyscan', ['-p', remotePort, '-H', remoteHost] ).then( ( result ) => {
-		fs.appendFileSync( '/home/runner/.ssh/known_hosts', result.stdout );
-	} );
 
+	let output = '';
+	await exec.exec( 'ssh-keyscan', ['-p', remotePort, '-H', remoteHost], {
+		listeners: {
+			stdout: ( data ) => {
+				output += data.toString();
+			}
+		}
+	} );
+	fs.appendFileSync( '/home/runner/.ssh/known_hosts', output );
 } )();
