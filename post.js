cc78427b35019a9d6feb1697be46e63d61257d5c
Matias Saggiorato <matiassaggiorato@gmail.com>
2023-05-29 19:41:10 -0300

    oopsie

--- post.js
+++ post.js
@@ -1,4 +1,6 @@
 ( async function () {
+	const exec = require( '@actions/exec' );
+	const fs = require( 'fs' );
 	var i = 1, keyPath;
 	keyPath = '/home/runner/.ssh/github_actions_' + i;
 	while( fs.existsSync( keyPath ) ) {
