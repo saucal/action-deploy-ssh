33aea230486c5b54fe6e7f2984999a6b55410145
Kostas Seresiotis <kostas@saucal.com>
2023-11-10 19:47:20 +0200

    attempt to run the cmd

--- main.js
+++ main.js
@@ -182,6 +182,14 @@
 		}
 	}
 
+	if ( prePushRemoteCmd ) {
+		console.log( 'Running pre-push remote command.' );
+	
+		const sshCommand = 'ssh ' + remoteTarget + ' ' + shellParams.join( ' ' ) +  ' "' + prePushRemoteCmd + '"';
+		console.log( 'sshCommand: ' + sshCommand );
+		var { code, processedFiles, bufferPath } = await runCommand( sshCommand );
+	}
+
 	var { code, processedFiles, bufferPath } = await runCommand( rsyncCommand );
 	console.log( '::endgroup::' );
 } )();
