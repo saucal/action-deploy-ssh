73fafd655ed229bb6a5b0bdc078ef248ae732f6a
Matias Saggiorato <matiassaggiorato@gmail.com>
2025-06-28 02:14:49 -0300

    Simplify diff output

--- consistency-diff.sh
+++ consistency-diff.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 cd "${PATH_DIR}" || exit 1;
 git add -A . > /dev/null 2>&1
-git --no-pager diff -R -M HEAD ':(exclude)saucal-mu-plugins'
+git --no-pager diff -R -M --name-status HEAD ':(exclude)saucal-mu-plugins' | while read status file; do
+  if [ "$status" = "M" ]; then
+    git --no-pager diff -R -M HEAD "$file"
+  else
+    echo diff --git --simple "$status" "$file"
+  fi
+done
